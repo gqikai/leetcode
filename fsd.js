fff8c4c5fbcdcac10cd6aae8090de23fe71be197
gaoqikai <439673129@qq.com>
2016-05-15 10:07:24 +0800

    learn setTimeout

--- fsd.js
+++
@@ -1,7 +0,0 @@
-var a = function () {
-    
-}
-
-var b = a;
-
-console.log(a === b);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 ;(function () {
-    require(['QuickSort','ToStackInAQueue'], function (Quick,ToQueue) {
-
+    require(['QuickSort','EventUtil'], function (Quick,EventUtil) {
         console.log(Quick([72,6,57,88,60,42,83,73,48,85]));
     })
 })();
